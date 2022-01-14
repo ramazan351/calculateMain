2b4052baeb9ccc57ae69d7abc7965f167452cb73
ramazan351 <31069732+ramazan351@users.noreply.github.com>
2022-01-14 14:51:36 +0300

    added some property

--- app/src/main/java/com/ramazanaltun/calculatemain/MainActivity.kt
+++ app/src/main/java/com/ramazanaltun/calculatemain/MainActivity.kt
@@ -7,14 +7,18 @@ import android.view.LayoutInflater
 import android.widget.Toast
 import com.ramazanaltun.calculatemain.databinding.ActivityMainBinding
 
+
 class MainActivity : AppCompatActivity() {
     private lateinit var viewBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         viewBinding= ActivityMainBinding.inflate(LayoutInflater.from(this))
         setContentView(viewBinding.root)
-        viewBinding.btnCalcul.setOnClickListener {
-            Log.d("ramazan","clicked")
+        viewBinding.btnSave.setOnClickListener {
+            // we convert xml code to java with inflater
+            var inflater=LayoutInflater.from(this)
+            var mylessonView= inflater.inflate(R.layout.new_lesson_layout,null)
+            viewBinding.srclLessonView.addView(mylessonView)
 
         }
     }
